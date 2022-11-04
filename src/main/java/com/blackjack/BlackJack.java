7b4f593b0151b4cf1076a4f53f08fa53c549f6bc
Oskar R <o.rajzner@gmail.com>
2022-11-04 18:45:24 +0100

    version not complited, saved for future use

--- src/main/java/com/blackjack/BlackJack.java
+++ src/main/java/com/blackjack/BlackJack.java
@@ -1,26 +1,44 @@
 package com.blackjack;
 import javafx.application.Application;
-import javafx.scene.Group;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class BlackJack extends Application {
-
+    private Image imageback = new Image("file:src/main/resources/pokerbackground.jpg");
+    private Image card = new Image("file:src/main/resources/2-pik-wino.png");
+    private FlowPane cards = new FlowPane(Orientation.HORIZONTAL);
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
-        Group root = new Group();
-        Scene scene = new Scene(root, 300, 300, Color.BLACK);
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        BackgroundImage backgroundImage = new BackgroundImage(imageback, BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        Background background = new Background(backgroundImage);
 
-        Rectangle r = new Rectangle(25,25,250,250);
-        r.setFill(Color.BLUE);
+        GridPane grid = new GridPane();
+        grid.setAlignment(Pos.CENTER);
+        grid.setPadding(new Insets(11.5, 12.5, 13.5, 14.5));
+        grid.setHgap(5.5);
+        grid.setVgap(5.5);
+        grid.setBackground(background);
 
-        root.getChildren().add(r);
+        ImageView img = new ImageView(card);
+        img.setFitHeight(125);
+        img.setPreserveRatio(true);
+        cards.getChildren().add(img);
+
+        grid.add(cards, 0, 0, 3, 1);
+
+        Scene scene = new Scene(grid, 1600, 900, Color.BLACK);
+
         primaryStage.setTitle("BlackJack");
         primaryStage.setScene(scene);
         primaryStage.show();
